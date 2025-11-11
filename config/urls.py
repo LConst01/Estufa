b1cc1e0e13fb3436c216b9811e06579f3bbb1a4e
LConst01 <cunhalucca26@gmail.com>
2025-11-11 13:37:16 -0300

    aula do dia 11.11

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatter =[
+urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('estufa.urls')),
+    path('', include('estufa.urls')),  
 ]
